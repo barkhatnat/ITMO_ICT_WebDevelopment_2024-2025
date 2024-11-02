a2d4295f4e77e605e2f85a67592796df4a9a0e60
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-11-02 22:36:47 +0300

    Практическая работа №2.2

--- students/k3339/practical_works/Barkhatova_Natalia/simple_django_web_project/django_project_Barkhatova/project_first_app/views.py
+++ students/k3339/practical_works/Barkhatova_Natalia/simple_django_web_project/django_project_Barkhatova/project_first_app/views.py
@@ -1,6 +1,10 @@
 from django.http import Http404
-from django.shortcuts import render
-from project_first_app.models import Owner
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+
+from project_first_app.forms import OwnerForm, CarForm
+from project_first_app.models import Owner, Car
 
 
 def detail(request, owner_id):
@@ -10,3 +14,51 @@ def detail(request, owner_id):
         raise Http404(
             "Owner does not exist")
     return render(request, 'owner.html', {'owner': p})
+
+
+def owner_list(request):
+    owners = Owner.objects.all()
+    return render(request, 'owner_list.html', {'owners': owners})
+
+
+class CarListView(ListView):
+    model = Car
+    template_name = 'car_list.html'
+    context_object_name = 'cars'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    context_object_name = 'car'
+
+
+class CarCreateView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'car_form.html'
+    success_url = reverse_lazy('car_list')
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'car_form.html'
+    success_url = reverse_lazy('car_list')
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_confirm_delete.html'
+    success_url = reverse_lazy('car_list')
+
+
+def add_owner(request):
+    if request.method == 'POST':
+        form = OwnerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('owner_list')
+    else:
+        form = OwnerForm()
+    return render(request, 'add_owner.html', {'form': form})
