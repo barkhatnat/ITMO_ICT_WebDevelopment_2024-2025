4d278fccc7787e5c426f519b3c82c781af441958
Natalia Barkhatova <natabarkhatova@yandex.ru>
2025-01-19 18:47:40 +0300

    modal

---
+++ students/k3339/Barkhatova_Natalia/Lr4/src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="modal-overlay" @click="close">
+    <div class="modal-content" @click.stop>
+      <button class="close-btn" @click="close">X</button>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  width: 60%;
+  height: 60%;
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
